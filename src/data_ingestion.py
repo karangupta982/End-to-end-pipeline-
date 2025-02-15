d074d99133088586d757deda16e0d0f1192cd68f
Karan Gupta <gupta.karan.gh@gmail.com>
2025-02-15 13:20:30 +0530

    components done

---
+++ src/data_ingestion.py
@@ -0,0 +1,108 @@
+import pandas as pd
+import os
+from sklearn.model_selection import train_test_split
+import logging
+import yaml
+
+log_dir = 'logs'
+os.makedirs(log_dir,exist_ok=True)
+
+
+logger = logging.getLogger('data_ingestion')
+logger.setLevel('DEBUG')
+
+console_handler = logging.StreamHandler()
+console_handler.setLevel('DEBUG')
+
+log_file_path = os.path.join(log_dir,'data_ingestion.log')
+file_handler = logging.FileHandler(log_file_path)
+file_handler.setLevel('DEBUG')
+
+
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+console_handler.setFormatter(formatter)
+file_handler.setFormatter(formatter)
+
+logger.addHandler(console_handler)
+logger.addHandler(file_handler)
+
+
+
+def load_params(params_path: str) ->str:
+    """Load parameter written in YAML file"""
+
+    try:
+        with open(params_path,'r') as file:
+            params = yaml.safe_load(file)
+        logger.debug("parameters Loaded from %s",params_path)   #%s -> params_path
+        return params
+    
+    except FileNotFoundError:
+        logger.error('File not found: %s',params_path)
+        raise
+
+    except yaml.YAMLError as e:
+        logger.error("Error occured in yaml %s",e)
+        raise
+
+    except Exception as e:
+        logger.debug("Unexpected error occured %s",e)
+        raise
+
+
+def load_data(data_url:str)->pd.DataFrame:
+    """Loading Data from a CSV file"""
+    try:
+        df = pd.read_csv(data_url)
+        logger.debug("data loaded from %s",data_url)
+        return df
+    except pd.errors.ParserError as e:
+        logger.error("failed to parse the CSV file: %s",e)
+        raise
+    except Exception as e:
+        logger.error("unexpected error occured while loading the data %s",e)
+        raise
+
+def preprocess_data(df: pd.DataFrame) -> pd.DataFrame:
+    """Preprocessing of data is done"""
+    try:
+        df.drop(columns=['Unnamed: 2', 'Unnamed: 3', 'Unnamed: 4'],inplace=True)
+        df.rename(columns={'v1': 'target', 'v2': 'text'} ,inplace=True)
+        logger.debug("Data preprocessing Completed")
+        return df
+    except KeyError as e:
+        logger.error("Missing column in the dataset %s",e)
+        raise
+    except Exception as e:
+        logger.error("unexcpted error occured while preprocessing %s",e)
+        raise
+
+def save_data(train_data:pd.DataFrame, test_data:pd.DataFrame, data_path:str)->None:
+    """save the train and test dataset"""
+    try:
+        raw_data_path = os.path.join(data_path,'raw')
+        os.makedirs(raw_data_path,exist_ok=True)
+        train_data.to_csv(os.path.join(raw_data_path,'train.csv'),index=False)   #index column will not be inserted in dataframe as a data
+        test_data.to_csv(os.path.join(raw_data_path,'test.csv'),index=False)
+    except Exception as e:
+        logger.error("Unexpected error occured while saving the data %s",e)
+        raise
+
+def main():
+    try:
+        params = load_params(params_path='params.yaml')
+        test_size = params['data_ingestion']['test_size']
+
+        data_path = 'https://raw.githubusercontent.com/vikashishere/Datasets/main/spam.csv'
+        df = load_data(data_url=data_path)
+        final_df = preprocess_data(df)
+
+        train_data,test_data = train_test_split(final_df,test_size=test_size,random_state=3)
+        save_data(train_data=train_data,test_data=test_data,data_path='./data')
+    except Exception as e:
+        logger.error("Failed to completed the data ingestion %s",e)
+        print(f"error: {e}")
+
+
+if __name__ == '__main__':
+    main()
